96e07e98d67349907238b11479fb0eabfcd36634
Otabekov Shohjaxon <118268074+Shohjaxon-098@users.noreply.github.com>
2025-07-24 14:08:04 +0500

    Revert "."

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,7 @@
+abstract class AppImages {
+  static const String home = "assets/icons/home.svg";
+  static const String card = "assets/icons/card.svg";
+  static const String category = "assets/icons/category.svg";
+  static const String favourite = "assets/icons/favourite.svg";
+  static const String profile = "assets/icons/profile.svg";
+}
